9594187c006099f1efff3f9791f1fd9ca199089f
tksuns12 <tksuns@gmail.com>
2020-05-14 15:25:06 +0900

    이메일 계정 생성 시 이메일과 패스워드 형식 검사 추가

--- lib/pages/page_email_sign_in.dart
+++ lib/pages/page_email_sign_in.dart
@@ -15,27 +15,69 @@ class _EmailSignInState extends State<EmailSignIn> {
 
   @override
   Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
     return Container(
       child: Material(
         child: Stack(children: <Widget>[
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text('Our Logo'),
-              Padding(
-                padding: const EdgeInsets.only(top: 200),
-                child: Row(
+          Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('Our Logo'),
+                Padding(
+                  padding: const EdgeInsets.only(top: 200),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(Icons.email),
+                      Column(
+                        children: <Widget>[
+                          SizedBox(
+                            width: 250,
+                            child: TextFormField(
+                              keyboardType: TextInputType.emailAddress,
+                              onChanged: (text) {
+                                email = text;
+                              },
+                              validator: (value) {
+                                Pattern emailPattern = r'^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$';
+                                RegExp emailRegex = RegExp(emailPattern);
+                                if (!emailRegex.hasMatch(value)) {
+                                  return "이메일 주소 형식이 안 맞습니다.";
+                                } else {
+                                  return null;
+                                }
+                              },
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    Icon(Icons.email),
+                    Icon(Icons.vpn_key),
                     Column(
                       children: <Widget>[
                         SizedBox(
                           width: 250,
-                          child: TextField(
-                            keyboardType: TextInputType.emailAddress,
+                          child: TextFormField(
+                            maxLength: 12,
+                            obscureText: true,
                             onChanged: (text) {
-                              email = text;
+                              password = text;
+                            },
+                            validator: (value) {
+                              Pattern passwordPattern = r'^(?=.*[a-z])(?=.*[0-9])(?=.{8,12})$';
+                              RegExp passwordRegex = RegExp(passwordPattern);
+                              if (!passwordRegex.hasMatch(value)) {
+                                return '비밀번호는 영문, 숫자 8-12 자리입니다.';
+                              } else {
+                                return null;
+                              }
                             },
                           ),
                         ),
@@ -43,62 +85,42 @@ class _EmailSignInState extends State<EmailSignIn> {
                     )
                   ],
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Icon(Icons.vpn_key),
-                  Column(
-                    children: <Widget>[
-                      SizedBox(
-                        width: 250,
-                        child: TextField(
-                          maxLength: 12,
-                          obscureText: true,
-                          onChanged: (text) {
-                            password = text;
-                          },
-                        ),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 20),
-                child: FlatButton(
-                  child: Container(
-                    child: Text('로그인'),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: FlatButton(
+                    child: Container(
+                      child: Text('로그인'),
+                    ),
+                    onPressed: () async {
+                      setState(() {
+                        isLoading = true;
+                      });
+                      await _authorization
+                          .signInEmail(
+                          context: context, email: email, password: password).then((value) {
+                            setState(() {
+                              isLoading = false;
+                            });
+                      });
+                    },
                   ),
-                  onPressed: () async {
-                    setState(() {
-                      isLoading = true;
-                    });
-                    await _authorization
-                        .signInEmail(
-                        context: context, email: email, password: password).then((value) {
-                          setState(() {
-                            isLoading = false;
-                          });
-                    });
-                  },
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 50),
-                child: FlatButton(
-                  child: Text('계정 생성'),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => CreateAccountPage(),
-                      ),
-                    );
-                  },
+                Padding(
+                  padding: const EdgeInsets.only(top: 50),
+                  child: FlatButton(
+                    child: Text('계정 생성'),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => CreateAccountPage(),
+                        ),
+                      );
+                    },
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
           Visibility(child: Center(child: CircularProgressIndicator()), visible: isLoading,),
         ],),
